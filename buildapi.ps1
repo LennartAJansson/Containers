8dd5e8e3d90171b51b14dc18f960f8f98b129220
lennartajansson <lennart.a.jansson@outlook.com>
2022-02-22 08:37:56 +0100

    Intermediate commit

--- buildapi.ps1
+++ buildapi.ps1
@@ -1,5 +1,8 @@
+$buildVersion = curl.exe "https://localhost:7176/api/Binaries/RevisionInc/workloadsapi"  | ConvertFrom-Json
+$semanticVersion = $buildVersion.buildVersion.semanticVersion
+$semanticVersion
 docker build -f .\WorkloadsApi\Dockerfile --force-rm -t workloadsapi .
-docker tag workloadsapi:latest $env:registryhost/workloadsapi:latest
-docker push $env:registryhost/workloadsapi:latest
+docker tag workloadsapi:latest $env:registryhost/workloadsapi:$semanticVersion
+docker push $env:registryhost/workloadsapi:$semanticVersion
 
 # Remember, ":latest" is not optimal, we need to cover versioning of the images somehow
--- buildprojector.ps1
+++ buildprojector.ps1
@@ -1,5 +1,8 @@
+$buildVersion = curl.exe "https://localhost:7176/api/Binaries/RevisionInc/workloadsprojector"  | ConvertFrom-Json
+$semanticVersion = $buildVersion.buildVersion.semanticVersion
+$semanticVersion
 docker build -f .\WorkloadsProjector\Dockerfile --force-rm -t workloadsprojector .
-docker tag workloadsprojector:latest $env:registryhost/workloadsprojector:latest
-docker push $env:registryhost/workloadsprojector:latest
+docker tag workloadsprojector:latest $env:registryhost/workloadsprojector:$semanticVersion
+docker push $env:registryhost/workloadsprojector:$semanticVersion
 
 # Remember, ":latest" is not optimal, we need to cover versioning of the images somehow
