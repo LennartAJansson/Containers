b6696ae8400acc5a5f461a17ea0234a18d6957f9
lennartajansson <lennart.a.jansson@outlook.com>
2022-02-23 16:01:49 +0100

    Delete all images works but it doesn't remove repository folder which means they will still show up in _catalog

--- RegClean.ps1
+++ RegClean.ps1
@@ -7,11 +7,15 @@ foreach($repositoryName in $registry.repositories)
 	foreach($tag in $repositoryTags.tags)
 	{
 		$repositoryName + " - " + $tag
-		$response = Invoke-WebRequest -Uri http://localhost:5000/v2/$repositoryName/manifests/$tag -Method HEAD
+		$response = Invoke-WebRequest -Headers @{'Accept' = 'application/vnd.docker.distribution.manifest.v2+json'} -Uri http://localhost:5000/v2/$repositoryName/manifests/$tag -Method HEAD
 		$digest = $response.Headers["Docker-Content-Digest"]
 		"Trying to delete " + $digest
-		curl.exe -v -s -H "Accept: application/vnd.docker.distribution.manifest.v2+json" "http://localhost:5000/v2/$repositoryName/manifests/$digest"
+		curl.exe -v -s -H "Accept: application/vnd.docker.distribution.manifest.v2+json" -X DELETE "http://localhost:5000/v2/$repositoryName/manifests/$digest"
 	}
 }
 
-#docker exec registry bin/registry garbage-collect /etc/docker/registry/config.yml
+docker exec registry bin/registry garbage-collect /etc/docker/registry/config.yml
+
+#Only way to delete repository folders is to do following:
+#docker exec registry sudo rm -rf /var/lib/registry/docker/v2/repositories/workloadsapi
+#docker exec registry sudo rm -rf /var/lib/registry/docker/v2/repositories/workloadsprojector
