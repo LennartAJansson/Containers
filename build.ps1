a5f455881bd68a8b6b096046db930f53e4366275
lennartajansson <lennart.a.jansson@outlook.com>
2022-04-01 11:07:27 +0200

    Brought scripts together

--- build.ps1
+++ build.ps1
@@ -1,3 +1,10 @@
+$alive = curl -s "http://buildversion.local:8081/Ping" -H "accept: text/plain"
+if($alive -eq "pong!")
+{
+	. (".\BuildWithVersioning.ps1")
+	return
+}
+
 foreach($name in @("workloadsapi", "workloadsprojector", "buildversion", "cronjob"))
 {
 	$semanticVersion = "latest"
--- deploy.ps1
+++ deploy.ps1
@@ -1,9 +1,15 @@
+$alive = curl -s "http://buildversion.local:8081/Ping" -H "accept: text/plain"
+if($alive -eq "pong!")
+{
+	. (".\DeployWithVersioning.ps1")
+	return
+}
+
 foreach($name in @("workloadsapi", "workloadsprojector", "buildversion", "cronjob"))
 {
 	$semanticVersion = "latest"
 	"Current deploy: ${name}:${semanticVersion}"
 	
-	#kubectl delete -k ./deploy/${name}
 	kubectl apply -k ./deploy/${name}
 
 	curl.exe -X POST -g "http://prometheus.local:8081/api/v1/admin/tsdb/delete_series?match[]={app='${name}'}"
