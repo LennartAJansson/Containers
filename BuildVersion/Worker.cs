ad689305f93d732b43d722f6c9c5a1a237bcda99
lennartajansson <lennart.a.jansson@outlook.com>
2022-04-14 13:56:59 +0200

    Adding healthz for BuildVersion and CountriesApi

---
+++ BuildVersion/Worker.cs
@@ -0,0 +1,21 @@
+﻿using BuildVersion.Health;
+
+public class Worker : BackgroundService
+{
+    private readonly ILogger<Worker> logger;
+    private readonly WorkerWitness witness;
+
+    public Worker(ILogger<Worker> logger, WorkerWitness witness)
+    {
+        this.logger = logger;
+        this.witness = witness;
+    }
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            witness.LastExecution = DateTime.Now;
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
