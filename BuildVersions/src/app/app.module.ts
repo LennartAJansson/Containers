3cad4054547259bdbb3f27909724b10a7ca70e94
Lennartajansson <lennart.a.jansson@outlook.com>
2022-05-03 20:46:12 +0200

    Added menu and bootstrap

--- BuildVersions/src/app/app.module.ts
+++ BuildVersions/src/app/app.module.ts
@@ -1,18 +1,25 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { ListComponent } from './list/list.component';
+import { HomeComponent } from './home/home.component';
+import {
+  BuildVersionsClient,
+  API_BASE_URL,
+} from './services/BuildVersionsClient';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent, NavMenuComponent, HomeComponent, ListComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [
+    { provide: API_BASE_URL, useValue: environment.url },
+    BuildVersionsClient,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
